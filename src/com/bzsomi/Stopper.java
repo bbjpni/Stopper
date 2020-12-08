4d30a6aec8beba28fc557cd9cb53b684a33fef58
Zsombor <brozsek.zsombor@petrik.hu>
2020-12-08 19:05:13 +0100

    TELJESEN MUKODO PROGRAM

--- src/com/bzsomi/Stopper.java
+++ src/com/bzsomi/Stopper.java
@@ -9,14 +9,17 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Stopper extends JFrame {
     JPanel mainPanel;
-    JLabel szamlap;
+    JLabel szamlap, pillanat;
     JButton start, time;
     LocalTime idopont;
     boolean elinditva;
+    Duration elteltIdo;
+    ArrayList<Duration> reszidok;
 
     public Stopper(){
         init();
@@ -30,26 +33,69 @@ public class Stopper extends JFrame {
         this.setLayout(null);
 
         elinditva = false;
+        elteltIdo = Duration.ZERO;
+        reszidok = new ArrayList<>();
 
         start = new JButton("Start");
         start.setBounds(20,20,150,30);
 
         time = new JButton("Reset");
         time.setBounds(20,70,150,30);
+        time.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (time.getText() == "Részidő") {
+                    reszidok.add(elteltIdo.plus( (Duration.between(idopont, LocalTime.now())) ));
+                    String text = "<html>";
+                    for (Duration ido : reszidok){
+                        text += "<p>" + String.format("%02d:%02d.%03d",
+                                ido.toMinutesPart(), ido.toSecondsPart(), ido.toMillisPart()) + "</p>";
+                    }
+                    text += "</html>";
+                    pillanat.setText(text);
+                }
+                else {
+                    szamlap.setText("00:00.000");
+                    elteltIdo = Duration.ZERO;
+                    elinditva = false;
+                    reszidok = new ArrayList<>();
+                    pillanat.setText("");
+                    //start.setText("Start");
+                    //time.setText("Reset");
+                }
+            }
+        });
 
         szamlap = new JLabel("00:00.000");
         szamlap.setBounds(190, 20, 370, 100);
         szamlap.setFont(new Font(Font.SERIF, Font.BOLD, 70));
 
+        pillanat = new JLabel("");
+        pillanat.setBounds(190, 140, 370, 440);
+        pillanat.setFont(new Font(Font.SERIF, Font.BOLD, 12));
+
         mainPanel = (JPanel) (this.getContentPane());
         mainPanel.add(start);
         mainPanel.add(time);
         mainPanel.add(szamlap);
+        mainPanel.add(pillanat);
 
         start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                start();
+                if (!t.isRunning()) {
+                    t.start();
+                    start.setText("Stop");
+                    time.setText("Részidő");
+                }
+                else {
+                    t.stop();
+                    Duration temp = (Duration.between(idopont, LocalTime.now()));
+                    elteltIdo = elteltIdo.plus(temp);
+                    start.setText("Start");
+                    time.setText("Reset");
+                    elinditva = !elinditva;
+                }
             }
         });
 
@@ -63,15 +109,16 @@ public class Stopper extends JFrame {
                     idopont = LocalTime.now();
                     elinditva = !elinditva;
                 }
-                Duration temp = Duration.between(idopont, LocalTime.now());
-                int p =  temp.toMinutesPart();
-                int mp =  temp.toSecondsPart();
-                int szmp = temp.toMillisPart();
-                szamlap.setText(String.format("%02d:%02d.%03d", p, mp, szmp));
+                else {
+
+                    Duration temp = Duration.between(idopont, LocalTime.now());
+                    temp = temp.plus(elteltIdo);
+                    int p =  temp.toMinutesPart();
+                    int mp =  temp.toSecondsPart();
+                    int szmp = temp.toMillisPart();
+                    szamlap.setText(String.format("%02d:%02d.%03d", p, mp, szmp));
+                }
+
         }
     });
-
-    private void start(){
-        t.start();
-    }
 }
